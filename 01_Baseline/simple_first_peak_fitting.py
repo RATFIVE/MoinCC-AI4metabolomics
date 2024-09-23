1a7f4ae034d00a469075ecc8d909b1d8f906dc92
Tom Ruge <tom.ruge@student.fh-kiel.de>
2024-09-23 17:52:46 +0200

    Successfull first fit

--- 01_Baseline/simple_first_peak_fitting.py
+++ 01_Baseline/simple_first_peak_fitting.py
@@ -51,28 +51,84 @@ def extract_ppm_all(meta_df, file_name):
     names.append('Water')
     return positions, names
 
-def single_plot(df, y, ppm_lines, names, file_name):
-    plt.figure(figsize=(10, 6))
-    plt.plot(df[:, 0], df[:, y])
-    plt.xlabel('Chemical Shift (ppm)')
-    plt.ylabel('Intensity')
-    plt.title(f'NMR Spectrum of {file_name}')
-    # add vertical lines for the ppm
-    for i in range(len(ppm_lines)):
-        plt.axvline(x=ppm_lines[i], linestyle='--', label=names[i])
-    plt.legend()
-    plt.show()
 
+def plot_single(x, y_fits, positions, names, file_name, df):
+    for i in range(y_fits.shape[1] - 1):
+        plt.figure(figsize=(10, 6))
+        plt.plot(x, y_fits[:, i], label='Fit')
+        print(df.shape)
+        plt.plot(df.iloc[:, 0], df.iloc[:, i + 1], label='Data')
+        
+        plt.xlabel('Chemical Shift (ppm)')
+        plt.ylabel('Intensity')
+        plt.title(f'NMR Spectrum of {file_name}')
+        
+        colors = ['r', 'g', 'b', 'y', 'm', 'c']
+        # Add vertical lines for the ppm
+        for j in range(len(positions)):
+            plt.axvline(x=positions[j], linestyle='--', label=names[j], color=colors[j % len(colors)])
+        
+        # Add legend to avoid repetition
+        plt.legend()
+        
+        # Save the plot
+        plt.savefig(f'output/{file_name}_{i}.png')
+        plt.close()
+
+def plot_time_dependece(y_fits, positions, names, popt):
+    # the area of the indivudal peaks is the concentration with some kind of linear transformation on top.
+    area_per_peak = np.zeros((y_fits.shape[1], len(positions)))
+    print(area_per_peak.shape)
+    for i in range(y_fits.shape[1]):
+        for j in range(len(positions)):
+            area_per_peak[i, j] = popt[i, 3*j + 2] * popt[i, 3*j+1]
+    
+    print(popt)
+    print('Shape of area_per_peak:', area_per_peak.shape)
+    print('Shape of y_fits:', y_fits.shape)
+
+    time_points = range(y_fits.shape[1])
+
+    # scatter plot of time evolution of the peak areas
+    plt.figure(figsize=(10, 6))
+    colors = ['r', 'g', 'b', 'y', 'm', 'c']
+    for i in range(len(positions)):
+        plt.scatter(time_points, area_per_peak[:, i], label=names[i], color=colors[i % len(colors)])
+    plt.xlabel('Time')
+    plt.ylabel('Area')
+    plt.title('Time evolution of peak areas')
+    plt.legend()
+    plt.savefig(f'output/Time_evolution_areas.png')
+    plt.close()    
 
 def main():
-    #file_names = get_file_names()
     #file_names = containing_string(file_names, 'Fumerate')
     file_name = 'FA_20240213_2H_yeast_Fumarate-d2_9.csv'
-    print(f'Processing {file_name}')
     df = pd.read_csv(f'../Data/{file_name}')
-    df = df.to_numpy()
-    ppm_lines, names = extract_ppm_all(meta_df, file_name)
-    single_plot(df, 40, ppm_lines, names, file_name)
+    # meta path independet of the OS
+    meta_path = Path('..', 'Data', 'Data_description.xlsx')
+    # meta_path = '../Data/Data_description.xlsx'
+    meta_df = pd.read_excel(meta_path)
+    # extract ppm lines and names
+    positions, names = extract_ppm_all(meta_df, file_name)
+    
+    y_fits = np.zeros((df.shape[0], df.shape[1]))
+    fit_params = np.zeros((df.shape[1], 12))
+    fit_errors = np.zeros((df.shape[1], 3))
+
+    for i in range(1, df.shape[1]):
+        print(f'Fitting column {i/df.shape[1]*100:.2f}%')
+        # perform fitting
+        x = df.iloc[:,0]
+        y = df.iloc[:,i]
+        popt, pcov = curve_fit(grey_spectrum, x, y, p0 = [positions, [0.1]*len(positions), [1000]*len(positions)], maxfev=10000)
+        y_fits[:,i-1] = grey_spectrum(x, *popt)
+
+        fit_params[i-1] = popt
+        #fit_errors[i-1] = np.sqrt(np.diag(pcov))
+    #plot_single(x, y_fits, positions, names, file_name, df)
+    plot_time_dependece(y_fits, positions, names, fit_params)
+
 
 def lorentzian(x, x0, gamma, A):
     '''
@@ -81,21 +137,23 @@ def lorentzian(x, x0, gamma, A):
     gamma is the width
     A is the amplitude
     '''
-    return A * gamma**2 / ((x - x0)**2 + gamma**2)
+    return A * gamma / ((x - x0)**2 + gamma**2)
 
-def grey_spectrum(x, x0a, gammaa, Aa, x0b, gammab, Ab, x0c, gammac, Ac, x0d, gammad, Ad):
-    return lorentzian(x, x0a, gammaa, Aa) + lorentzian(x, x0b, gammab, Ab) + lorentzian(x, x0c, gammac, Ac) + lorentzian(x, x0d, gammad, Ad)
+def grey_spectrum(x, *params):
+    '''
+    x is the independent variable array
+    params should be a flattened list of x0, gamma, and A values
+    '''
+    n = len(params) // 3
+    x0 = params[:n]
+    gamma = params[n:2*n]
+    A = params[2*n:3*n]
+    
+    y = np.zeros(len(x))
+    for i in range(n):
+        y += lorentzian(x, x0[i], gamma[i], A[i])
+    return y
 
-def fit_lorentzian(x, y, x0s):
-    # 4 lorentz function are fitted now
-    # initial x0 value sfrom metadata
-    popt, pcov = curve_fit(grey_spectrum, x, y, p0=x0s)
 
-# get current working directory
-cwd = os.getcwd()
-# meta path independet of the OS
-meta_path = Path('..', 'Data', 'Data_description.xlsx')
-#meta_path = '../Data/Data_description.xlsx'
-meta_df = pd.read_excel(meta_path)
 
 main()
