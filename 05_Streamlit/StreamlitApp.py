f5a728c89edf9d3f67729ad2f4cea1ec90eb9954
Marco Banzhaf <generalpegasus@web.de>
2024-11-18 13:58:19 +0100

    starting a example layout

--- 05_Streamlit/StreamlitApp.py
+++ 05_Streamlit/StreamlitApp.py
@@ -6,6 +6,16 @@ import subprocess
 import os
 import sys
 import psutil  # For checking running processes
+from pathlib import Path
+from PIL import Image
+import lorem
+from classes import *
+from example_pic import plot_picture
+
+
+loaddata = LoadData()
+
+peakfinder = PeakFinder()
 
 st.set_page_config(layout="wide", page_title="MoinCC - Application", page_icon=":shark:")
 
@@ -16,12 +26,17 @@ class StreamlitApp():
         self.fig2 = fig2
 
     def side_bar(self):
-        st.sidebar.title('Side Panel')
-        st.sidebar.markdown('This is the side panel')
+        st.sidebar.title('How to')
+        st.sidebar.markdown(
+            """
+            1. Upload the .CSV-File 
+            2. ...
+            3. ...
 
-        # Example widgets in the side panel
-        slider_value = st.sidebar.slider("Select a value", 0, 100, 50)
-        st.sidebar.write(f"Slider value: {slider_value}")
+            """
+                            )
+
+        
 
     def header(self):
         st.markdown(
@@ -29,47 +44,55 @@ class StreamlitApp():
                 # MoinCC - Application
             """
         )
+        st.file_uploader("Upload a the Metabolite Spectrum CSV")
 
     def columns(self):
         col1, col2 = st.columns([2, 3])
         
         with col1:
-            st.markdown("""
-        ## Sine Wave Plot
-        This is a simple Plotly figure showing a sine wave. The plot displays the relationship 
-        between the x-axis and y-axis, where the y-axis represents the sine of the values on the x-axis.
-        The sine wave oscillates between -1 and 1.
+            st.markdown(f"""
+        ## Figure 
+        This plot shows {lorem.paragraph()}
                         """)
             
         with col2:
-            st.plotly_chart(self.fig1)
+            #st.plotly_chart(self.fig1)
+            st.image(str(self.fig1), caption="Image", use_column_width=True)
+        
+        st.markdown(f"""
+                    ## Full Spectrum
+
+                    This Plot shows {lorem.paragraph()}
+                    """)
+        st.plotly_chart(self.fig2)
+
+
 
     def run(self):
         self.side_bar()
         self.header()
         self.columns()
 
-# Create a simple Plotly figure
-def create_plot():
-    x = np.linspace(1, 10, 1000)
-    y = np.log(x)
 
-    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines', name='Log Curve'))
-    fig.update_layout(title="Logarithmic Curve Example", xaxis_title="X", yaxis_title="Y")
-    return fig
+# ----------------------------------------------------------------------------------------------------
 
 
 
-def launch_streamlit_app():
-    # Running the Streamlit app using subprocess
-    script_path = os.path.abspath(sys.argv[0])  # Get the path of the current script
-    subprocess.Popen(["streamlit", "run", script_path])
 
 if __name__ == '__main__':
-    fig1 = create_plot()
 
+
+    file = str(Path('FA_20231123_2H Yeast_Fumarate-d2_12 .csv'))
+    substrates = loaddata.get_substrate_list(file)
+    metabolites = loaddata.get_metabolite_list(file)
+    df_list = loaddata.load_data(file)
+    df = pd.read_csv(df_list[0])
+    fig2 = plot_picture(df=df)
+
+    example_image_path = Path(r'/Users/marco/Documents/MoinCC-AI4metabolomics/05_Streamlit/example/FA_20240228_2H_yeast_fumarate-d2_4.csv_output/FA_20240228_2H_yeast_fumarate-d2_4.csv_time_dependence.png')
+    
     # Run Streamlit App
-    app = StreamlitApp(fig1)
+    app = StreamlitApp(fig1=example_image_path, fig2=fig2)
     app.run()
 
 
