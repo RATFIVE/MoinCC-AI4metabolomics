3ab2db38599ebf66ba0ec4cce3a73553df56fc89
Marco Banzhaf <generalpegasus@web.de>
2024-11-19 16:56:51 +0100

    Layout und Datenvisualisierung in app.py implementiert

--- app/app.py
+++ app/app.py
@@ -1,1 +1,32 @@
-import St
+import Layout
+import panel3_contour_plot as p3cp
+from pathlib import Path
+import pandas as pd
+from LoadData import *
+
+
+
+loaddata = LoadData()
+
+df_list = loaddata.load_data_list('FA_20231123_2H Yeast_Fumarate-d2_12 .csv')
+df = pd.read_csv(df_list[0])
+
+
+
+example_image_path = str(Path(r'/Users/marco/Documents/MoinCC-AI4metabolomics/app/example/FA_20240205_2H_yeast _Gluc-d2_5.csv_time_dependence.png'))
+
+
+fig3 = p3cp.ContourPlot(df=df)
+fig3 = fig3.plot()
+
+
+
+def main():
+    app = Layout.StreamlitApp(fig2=example_image_path,
+                              fig3=fig3)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
+
