c50019053e82807fda9e8d5dd741ef835c36165a
Tom Ruge <tom.ruge@student.fh-kiel.de>
2024-11-22 14:19:23 +0100

    Start implementing the output of Process4Panels.py

--- app/app.py
+++ app/app.py
@@ -6,25 +6,25 @@ from LoadData import *
 
 
 
-loaddata = LoadData()
+#loaddata = LoadData()
 
-df_list = loaddata.load_data_list('FA_20231123_2H Yeast_Fumarate-d2_12 .csv')
-df = pd.read_csv(df_list[0])
-print(df)
-
-
-
-example_image_path = str(Path(r'/Users/marco/Documents/MoinCC-AI4metabolomics/app/example/FA_20240205_2H_yeast _Gluc-d2_5.csv_time_dependence.png'))
+#df_list = loaddata.load_data_list('FA_20231123_2H Yeast_Fumarate-d2_12 .csv')
+#df = pd.read_csv(df_list[0])
+#print(df)
 
 
-fig3 = p3cp.ContourPlot(df=df)
-fig3 = fig3.plot()
+
+#example_image_path = str(Path(r'/Users/marco/Documents/MoinCC-AI4metabolomics/app/example/FA_20240205_2H_yeast _Gluc-d2_5.csv_time_dependence.png'))
+
+
+#fig3 = p3cp.ContourPlot(df=df)
+#fig3 = fig3.plot()
 
 
 
 def main():
-    app = Layout.StreamlitApp(fig2=example_image_path,
-                              fig3=fig3)
+    app = Layout.StreamlitApp()#fig2=example_image_path,
+                              #fig3=fig3)
     app.run()
 
 
---
+++ app/panel2_kinetic_plot.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+
