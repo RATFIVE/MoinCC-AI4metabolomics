2b907a539d8351f5e3dfcc127d3fd4aa9558adc4
Marco Banzhaf <generalpegasus@web.de>
2024-11-23 19:11:28 +0100

    adding noise plot

--- app/panel1_spectrum_plot.py
+++ app/panel1_spectrum_plot.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from LoadData import *
-from Layout import StreamlitApp
+#from Layout import StreamlitApp
 import streamlit as st
 
 loaddata = LoadData()
@@ -73,13 +73,33 @@ def plot_raw(df, i):
     fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='Raw'))
 
     fig.update_layout(
-        title='Lorentzian Plot',
+        title='Spectrum',
         xaxis_title='X',
         yaxis_title='Intensity',
         showlegend=False
     )
 
     return fig
+
+def get_lorentz(df, columns, xmin, xmax, length):
+    for col in columns:
+        if 'pos' in col:
+            pos = df[col]
+        if 'width' in col:
+            width = df[col]
+            
+        if 'amp' in col:
+            amp = df[col]
+            
+
+    # X-Werte für den Plot
+    x = np.linspace(xmin, xmax, length)
+
+    # Berechnen der Lorentz-Funktion für jeden Satz von Parametern
+
+    y = lorentzian(x, pos, width, amp)
+
+    return y
 
     
 
@@ -92,8 +112,7 @@ def plot_lorentz(df, fig, columns, xmin, xmax):
             
         if 'amp' in col:
             amp = df[col]
-            
-
+    
     # X-Werte für den Plot
     x = np.linspace(xmin, xmax, 1000)
 
@@ -108,7 +127,7 @@ def plot_lorentz(df, fig, columns, xmin, xmax):
 
     # Layout der Figur anpassen
     fig.update_layout(
-        title='Lorentzian Plot',
+        title='Lorentzians',
         xaxis_title='X',
         yaxis_title='Intensity',
         showlegend=False
@@ -118,13 +137,58 @@ def plot_lorentz(df, fig, columns, xmin, xmax):
     #fig.show()
     return fig
 
-def streamlit(fig1, fig2):
+def plot_noise(df, lorentz_array, frame):
+        # get the x and y of the i-th spectrum
+    df = df.iloc[:, [0, frame]]
+
+    # get the x data
+    x = df.iloc[:, 0]
+    y = np.zeros_like(x) 
+
+    # get the spectrum as y
+    raw = df.iloc[:, 1].values
+    
+
+    #print(raw)
+
+    y_max_array = np.maximum.reduce(lorentz_array, axis=0)
+    #print(y_max)
+
+    noise = raw - y_max_array
+
+    y_min = min(raw)
+    y_max = max(raw)
+
+    fig = go.Figure()
+
+    # Rauschkurve hinzufügen
+    fig.add_trace(go.Scatter(x=x, y=noise, mode='lines', name='Noise'))
+    #fig.add_trace(go.Scatter(x=x, y=raw, mode='lines', name='raw'))
+    #fig.add_trace(go.Scatter(x=x, y=y_max_array, mode='lines', name='lorenz'))
+
+    # Layout der Figur anpassen und y-Achsenbereich festlegen
+    fig.update_layout(
+        title='Noise Plot',
+        xaxis_title='X',
+        yaxis_title='Noise',
+        yaxis=dict(range=[y_min, y_max])  # y-Achsenbereich festlegen
+    )
+    
+    return fig
+    
+    
+
+    
+    
+
+def streamlit(fig1, fig2, fig3):
     st.plotly_chart(fig1)
     st.plotly_chart(fig2)
+    st.plotly_chart(fig3)
 
 
 def main():
-    frame = 10
+    frame = 2
     file_name = 'FA_20231109_2H_yeast_Gluc-d2_5.ser.csv'
     data_list = loaddata.load_data_list(file_name)
     df_raw = pd.read_csv(data_list[0])
@@ -132,23 +196,14 @@ def main():
     xmax = df_raw.iloc[:, 0].max()
     fig1 = plot_raw(df_raw, frame)
     
-
+    # for plot 2
     fit_params = iterate_directory(
                       target_dirname=file_name + '_output', 
                       target_filename='fitting_params.csv')
     
     df = pd.read_csv(fit_params)
-    column_set = df.columns
-    column_set = [col.split('_') for col in column_set]
-    
-    # Bereinigen der Liste
-    column_set = [col[0] for col in column_set if col[0] != 'Time']
-
-    column_set = set(column_set)
 
 
-    # Beispielaufruf
-    numbers = ['3.844', '3.975'] # Have to get the number from DataDescription
     substrates = loaddata.get_substrate_list(file_name)
     
     print(substrates)
@@ -159,7 +214,7 @@ def main():
     fig_lorentz = go.Figure()
 
     for number in substrates:
-        extracted_df = extract_reacsubs_columns(df,number)
+        extracted_df = extract_reacsubs_columns(df, number)
         columns = extracted_df.columns
 
         plot_df = extracted_df.iloc[frame, :]
@@ -172,9 +227,25 @@ def main():
         plot_df = extracted_df.iloc[frame, :]
         fig2 = plot_lorentz(plot_df, fig_lorentz, columns, xmin, xmax)
 
-    #streamlit(fig1, fig2)
+    # for Noise Plot
+    lorentz_list = []
+    for number in substrates + metabolites:
+        extracted_df = extract_reacsubs_columns(df, number)
+        columns = extracted_df.columns
+        df_lorentz = extracted_df.iloc[frame, :]
+        length = df_raw.shape[0]
+        y = get_lorentz(df=df_lorentz, columns=columns, xmin=xmin, xmax=xmax, length=length)
+        lorentz_list.append(y)
+    lorentz_array = np.vstack(lorentz_list)
+    
+    fig3 = plot_noise(df_raw, lorentz_array, frame)
+    
+    
 
-    return (fig1, fig2)
+
+    #streamlit(fig1, fig2, fig3)
+
+    return (fig1, fig2, fig3)
     
 
 
