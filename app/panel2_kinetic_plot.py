76b278222401b7827bf5b48f41d64a0e67d78d58
Marco Banzhaf <generalpegasus@web.de>
2024-11-24 17:00:40 +0100

    add kinematic plot

--- app/panel2_kinetic_plot.py
+++ app/panel2_kinetic_plot.py
@@ -1,7 +1,23 @@
-import os
-from pathlib import Path
 import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
+import plotly.express as px
+from pathlib import Path
+import os
 
+class KineticPlot:
+    def __init__(self, path):   
+        self.path = path
+        # init path
+        self.basename = os.path.basename(self.path)
+        self.kin_fp = Path('output', os.path.basename(self.path) + '_output', 'kinetics.csv')
+        self.kin_df = pd.read_csv(self.kin_fp)
 
+    def plot(self):
+        fig = px.line(
+            self.kin_df,
+            x='time',
+            y=self.kin_df.columns[:-1],  # Select all columns except 'time' for y
+            labels={'value': 'Value', 'variable': 'Series'},
+            title=f"Kinetic Plot of {self.basename}"
+        )
+        return fig
+        
